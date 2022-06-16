0d908667787676c95f977ea300f47184c7dffcd1
arojas2412 <107580422+arojas2412@users.noreply.github.com>
2022-06-16 16:03:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+ 
+client.once('ready',() => {
+    console.log('HairResearcher is online!');
+});
+
+client.login('OTg2NzAyNzE3NzQz NDA3MTk0.GsewTN.ADWGBIDwdyOUzJHoT3ICy1nHk3SI-1eF8K92JM');
+
+
+  
